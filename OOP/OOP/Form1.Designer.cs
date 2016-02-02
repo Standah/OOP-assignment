7fcba4bc84b1a2d397a7e29f1266fdc343c48548
Standah <standardkake@hotmail.com>
2016-02-02 16:21:44 +0100

    Added button

--- OOP/OOP/Form1.Designer.cs
+++ OOP/OOP/Form1.Designer.cs
@@ -23,12 +23,33 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent () {
-            this.components = new System.ComponentModel.Container ();
+            this.btnSample = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSample
+            // 
+            this.btnSample.AccessibleName = "";
+            this.btnSample.Location = new System.Drawing.Point(81, 116);
+            this.btnSample.Name = "btnSample";
+            this.btnSample.Size = new System.Drawing.Size(75, 35);
+            this.btnSample.TabIndex = 0;
+            this.btnSample.Text = "Sample";
+            this.btnSample.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnSample);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
             }
 
         #endregion
+        private System.Windows.Forms.Button btnSample;
         }
     }
 
