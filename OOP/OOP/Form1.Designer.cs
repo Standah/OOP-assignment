82cc8453c0e846af61fc240deed865f8e16e215a
Standah <standardkake@hotmail.com>
2016-02-02 20:13:53 +0100

    Revert "Merged program, messed up"
    This reverts commit 00b54404fb32c064bbfa982fe2c7ffc65581146e.

--- OOP/OOP/Form1.Designer.cs
+++ OOP/OOP/Form1.Designer.cs
@@ -25,17 +25,17 @@
         private void InitializeComponent () {
             this.btnSample = new System.Windows.Forms.Button();
             this.mnuTop = new System.Windows.Forms.MenuStrip();
-            this.mnuItmFile = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuItmOperations = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuItmHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.grpSampling = new System.Windows.Forms.GroupBox();
-            this.txtSampling = new System.Windows.Forms.TextBox();
-            this.lblSampling = new System.Windows.Forms.Label();
             this.grpLogging = new System.Windows.Forms.GroupBox();
-            this.txtLogging = new System.Windows.Forms.TextBox();
-            this.lblLogging = new System.Windows.Forms.Label();
             this.btnLogging = new System.Windows.Forms.Button();
+            this.lblSampling = new System.Windows.Forms.Label();
+            this.lblLogging = new System.Windows.Forms.Label();
             this.grpSensors = new System.Windows.Forms.GroupBox();
+            this.txtSampling = new System.Windows.Forms.TextBox();
+            this.txtLogging = new System.Windows.Forms.TextBox();
             this.txtSensors = new System.Windows.Forms.TextBox();
             this.mnuTop.SuspendLayout();
             this.grpSampling.SuspendLayout();
@@ -52,38 +52,37 @@
             this.btnSample.TabIndex = 0;
             this.btnSample.Text = "Sample";
             this.btnSample.UseVisualStyleBackColor = true;
-            this.btnSample.Click += new System.EventHandler(this.btnSample_Click);
             // 
             // mnuTop
             // 
             this.mnuTop.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.mnuTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuItmFile,
-            this.mnuItmOperations,
-            this.mnuItmHelp});
+            this.fileToolStripMenuItem,
+            this.operationsToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.mnuTop.Location = new System.Drawing.Point(0, 0);
             this.mnuTop.Name = "mnuTop";
             this.mnuTop.Size = new System.Drawing.Size(580, 28);
             this.mnuTop.TabIndex = 1;
             this.mnuTop.Text = "menuStrip1";
             // 
-            // mnuItmFile
+            // fileToolStripMenuItem
             // 
-            this.mnuItmFile.Name = "mnuItmFile";
-            this.mnuItmFile.Size = new System.Drawing.Size(44, 24);
-            this.mnuItmFile.Text = "File";
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
             // 
-            // mnuItmOperations
+            // operationsToolStripMenuItem
             // 
-            this.mnuItmOperations.Name = "mnuItmOperations";
-            this.mnuItmOperations.Size = new System.Drawing.Size(94, 24);
-            this.mnuItmOperations.Text = "Operations";
+            this.operationsToolStripMenuItem.Name = "operationsToolStripMenuItem";
+            this.operationsToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.operationsToolStripMenuItem.Text = "Operations";
             // 
-            // mnuItmHelp
+            // helpToolStripMenuItem
             // 
-            this.mnuItmHelp.Name = "mnuItmHelp";
-            this.mnuItmHelp.Size = new System.Drawing.Size(53, 24);
-            this.mnuItmHelp.Text = "Help";
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
             // 
             // grpSampling
             // 
@@ -96,23 +95,7 @@
             this.grpSampling.TabIndex = 3;
             this.grpSampling.TabStop = false;
             this.grpSampling.Text = "Sampling";
-            // 
-            // txtSampling
-            // 
-            this.txtSampling.Location = new System.Drawing.Point(152, 32);
-            this.txtSampling.Name = "txtSampling";
-            this.txtSampling.ReadOnly = true;
-            this.txtSampling.Size = new System.Drawing.Size(100, 22);
-            this.txtSampling.TabIndex = 2;
-            // 
-            // lblSampling
-            // 
-            this.lblSampling.AutoSize = true;
-            this.lblSampling.Location = new System.Drawing.Point(7, 32);
-            this.lblSampling.Name = "lblSampling";
-            this.lblSampling.Size = new System.Drawing.Size(133, 17);
-            this.lblSampling.TabIndex = 1;
-            this.lblSampling.Text = "Next Sampling Time";
+            this.grpSampling.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // grpLogging
             // 
@@ -126,23 +109,6 @@
             this.grpLogging.TabStop = false;
             this.grpLogging.Text = "Logging";
             // 
-            // txtLogging
-            // 
-            this.txtLogging.Location = new System.Drawing.Point(152, 21);
-            this.txtLogging.Name = "txtLogging";
-            this.txtLogging.ReadOnly = true;
-            this.txtLogging.Size = new System.Drawing.Size(100, 22);
-            this.txtLogging.TabIndex = 3;
-            // 
-            // lblLogging
-            // 
-            this.lblLogging.AutoSize = true;
-            this.lblLogging.Location = new System.Drawing.Point(10, 22);
-            this.lblLogging.Name = "lblLogging";
-            this.lblLogging.Size = new System.Drawing.Size(126, 17);
-            this.lblLogging.TabIndex = 1;
-            this.lblLogging.Text = "Next Logging Time";
-            // 
             // btnLogging
             // 
             this.btnLogging.AccessibleName = "";
@@ -153,6 +119,24 @@
             this.btnLogging.Text = "Logging on File";
             this.btnLogging.UseVisualStyleBackColor = true;
             // 
+            // lblSampling
+            // 
+            this.lblSampling.AutoSize = true;
+            this.lblSampling.Location = new System.Drawing.Point(7, 32);
+            this.lblSampling.Name = "lblSampling";
+            this.lblSampling.Size = new System.Drawing.Size(133, 17);
+            this.lblSampling.TabIndex = 1;
+            this.lblSampling.Text = "Next Sampling Time";
+            // 
+            // lblLogging
+            // 
+            this.lblLogging.AutoSize = true;
+            this.lblLogging.Location = new System.Drawing.Point(10, 22);
+            this.lblLogging.Name = "lblLogging";
+            this.lblLogging.Size = new System.Drawing.Size(126, 17);
+            this.lblLogging.TabIndex = 1;
+            this.lblLogging.Text = "Next Logging Time";
+            // 
             // grpSensors
             // 
             this.grpSensors.Controls.Add(this.txtSensors);
@@ -162,6 +146,23 @@
             this.grpSensors.TabIndex = 5;
             this.grpSensors.TabStop = false;
             this.grpSensors.Text = "Sensor Values";
+            this.grpSensors.Enter += new System.EventHandler(this.groupBox1_Enter_1);
+            // 
+            // txtSampling
+            // 
+            this.txtSampling.Location = new System.Drawing.Point(152, 32);
+            this.txtSampling.Name = "txtSampling";
+            this.txtSampling.ReadOnly = true;
+            this.txtSampling.Size = new System.Drawing.Size(100, 22);
+            this.txtSampling.TabIndex = 2;
+            // 
+            // txtLogging
+            // 
+            this.txtLogging.Location = new System.Drawing.Point(152, 21);
+            this.txtLogging.Name = "txtLogging";
+            this.txtLogging.ReadOnly = true;
+            this.txtLogging.Size = new System.Drawing.Size(100, 22);
+            this.txtLogging.TabIndex = 3;
             // 
             // txtSensors
             // 
@@ -200,9 +201,9 @@
         #endregion
         private System.Windows.Forms.Button btnSample;
         private System.Windows.Forms.MenuStrip mnuTop;
-        private System.Windows.Forms.ToolStripMenuItem mnuItmFile;
-        private System.Windows.Forms.ToolStripMenuItem mnuItmOperations;
-        private System.Windows.Forms.ToolStripMenuItem mnuItmHelp;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.GroupBox grpSampling;
         private System.Windows.Forms.Label lblSampling;
         private System.Windows.Forms.GroupBox grpLogging;
--- OOP/OOP/Form1.cs
+++ OOP/OOP/Form1.cs
@@ -9,27 +9,21 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OOP {
-    static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main () {
-            Application.EnableVisualStyles ();
-            Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run (new Form1 ());
-
-            Sensor sens1 = new Sensor (0);
-            }
-        } // end Program
-
     public partial class Form1:Form {
         public Form1 () {
             InitializeComponent ();
             }
 
-        private void btnSample_Click (object sender, EventArgs e) {
-            txtSampling.Text = "Hello World!";
+        private void button1_Click (object sender, EventArgs e) {
+
             }
-        } // end Form1:Form
-    } // end OOP
+
+        private void groupBox1_Enter (object sender, EventArgs e) {
+
+            }
+
+        private void groupBox1_Enter_1 (object sender, EventArgs e) {
+
+            }
+        }
+    }
